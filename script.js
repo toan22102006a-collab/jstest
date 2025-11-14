c3e7beb4cc0ee32ec07879d7d6a1bdce4e89ddfd
toan22102006a-collab <toan22102006a@gmail.com>
2025-11-14 15:29:28 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function undo() {
   imageDiv.innerText = "Hover over an image below to display here.";
   imageDiv.style.backgroundImage = "url('')";
 }
+
+window.onload = function () {
+  console.log("Trang đã tải xong");
+
+  const previews = document.querySelectorAll(".preview");
+  for (let i = 0; i < previews.length; i++) {
+    previews[i].addEventListener("mouseover", function () {
+      upDate(this);
+    });
+    previews[i].addEventListener("mouseleave", undo);
+
+    // Accessibility: thêm sự kiện bàn phím
+    previews[i].addEventListener("focus", function () {
+      upDate(this);
+    });
+    previews[i].addEventListener("blur", undo);
+
+    // Đảm bảo tabindex đã được thêm
+    previews[i].setAttribute("tabindex", "0");
+  }
+};
